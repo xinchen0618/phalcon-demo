d7240635717d726a9b1c82618ec69016a9a35f3f
chenxin <chen.xin@hbtown.com>
2019-08-09 18:18:31 +0800

    remove redundancy

--- app/config/config.php
+++ app/config/config.php
@@ -3,8 +3,6 @@
  * Modified: prepend directory path of current file, because of this file own different ENV under between Apache and command line.
  * NOTE: please remove this comment.
  */
-defined('BASE_PATH') || define('BASE_PATH', getenv('BASE_PATH') ?: dirname(__DIR__, 2) . '');
-defined('APP_PATH') || define('APP_PATH', BASE_PATH . '/app');
 
 return new \Phalcon\Config([
     'database' => [
